8ae7145d47c166aea2463ff50c64aa3a85a5ce9a
Adaane <adaane.ahamada@hetic.net>
2018-01-07 17:35:50 +0100

    React Router handle Routing

--- src/index.js
+++ src/index.js
@@ -4,5 +4,16 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Router Declaration
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+ReactDOM.render((
+  <Router>
+    <Route path="/" component={App}/>
+  </Router>
+  ), document.getElementById('root'));
 registerServiceWorker();
