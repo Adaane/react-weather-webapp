75ac16570371af0d514238903779a25229eea39c
Adaane <adaane.ahamada@hetic.net>
2018-01-12 09:20:09 +0100

    Start with create-react-app

--- src/components/DayItem.js
+++ src/components/DayItem.js
@@ -14,4 +14,19 @@ function DayItem(props) {
   )
 }
 
+// const DayItem = ({props}) => {
+//   const icon = props.icon;
+
+//   return (
+//     <div className='dayContainer'>
+//       {/*<h2 className='subheader'>{props.day}</h2>*/}
+//       <img className='weather' src={'images/weather-icons/' + icon + '.svg'} alt='Weather' />
+//       <h2 className='subheader'>{props.weatherCondition.replace(/\b\w/g, l => l.toUpperCase())}</h2>
+//       <h2 className='subheader'>Température actuelle : {props.temp + '°C'}</h2>
+//       <h2 className='subheader'>Température minimale : {props.tempMin + '°C'}</h2>
+//       <h2 className='subheader'>Température maximal : {props.tempMax + '°C'}</h2>
+//     </div>
+//   )
+// }
+
 export default DayItem;
